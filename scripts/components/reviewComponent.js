bdbf550a12a5d18bb736793452ab2a4e0c9d0c76
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-04-25 19:32:40 +0200

    Reworked the code for the reviewComponent still lacking functionality

--- scripts/components/reviewComponent.js
+++ scripts/components/reviewComponent.js
@@ -1,48 +1,47 @@
 export const ReviewComponent = (review) => {
   return `
-    <article class="reviewComponentContainer">
+    <article class="review-component-container">
       <img
         src="/assets/pictures/morbius-poster.jpg"
         alt="MovieName+Poster"
-        class="moviePosterReviewComponent"
+        class="poster-review-component"
       />
-      <main>
-        <div class="profilePictureNameTitle">
+      <section class="review-content">
+        <div class="picture-username-movietitle">
           <img src="/assets/pictures/guy.png" alt="User Profile Picture" />
-          <h3>DrainedLeto69</h3>
-          <h2 class="movieTitleReviewComponent"><span>Morbius</span> (2022)</h2>
+          <a href="">DrainedLeto69</a>
+          <a href="" class="movie-title-review-component"
+            ><span>Morbius</span> (2022)</a
+          >
         </div>
         <p>
           Morbius is like if someone turned a Hot Topic clearance rack into a
           movie. Jared Leto’s performance is so lifeless, I’m convinced he
           actually method-acted becoming a vampire and sucked the energy out of
-          the entire script. Morbius is like if someone turned a Hot Topic
-          clearance rack into a movie. Jared Leto’s performance is so lifeless,
-          I’m convinced he actually method-acted becoming a vampire and sucked
-          the energy out of the entire script.
+          the entire script.
         </p>
-      </main>
-      <div class="likeCommentShareRating">
-        <button>
-          <small>12k</small>
-          <img src="/assets/icons/heart.svg" alt="Like Button" />
-        </button>
-        <button>
-          <small>10</small>
-          <img src="/assets/icons/comment.svg" alt="Comment Button" />
-        </button>
-        <button>
-          <img src="/assets/icons/share.svg" alt="Share Button" />
-        </button>
-        <div class="rating">
-          <label for="Rating">Rating:</label>
-          <span class="rate-button"></span>
-          <span class="rate-button"></span>
-          <span class="rate-button"></span>
-          <span class="rate-button"></span>
-          <span class="rate-button"></span>
+        <div class="like-comment-share-rating">
+          <button>
+            <small>12k</small>
+            <img src="/assets/icons/heart.svg" alt="Like Button" />
+          </button>
+          <button>
+            <small>10</small>
+            <img src="/assets/icons/comment.svg" alt="Comment Button" />
+          </button>
+          <button>
+            <img src="/assets/icons/share.svg" alt="Share Button" />
+          </button>
+          <div class="rating">
+            <label for="Rating">Rating:</label>
+            <span class="rate-button"></span>
+            <span class="rate-button"></span>
+            <span class="rate-button"></span>
+            <span class="rate-button"></span>
+            <span class="rate-button"></span>
+          </div>
         </div>
-      </div>
+      </section>
     </article>
       `;
 };
