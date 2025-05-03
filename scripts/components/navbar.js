79005c81034954cf923f126a107fc666ed2f4c97
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-05-03 11:03:05 +0200

    Made the navbar into a component

---
+++ scripts/components/navbar.js
@@ -0,0 +1,76 @@
+export function NavbarComponent() {
+  const navbarComponent = document.createElement("div");
+  navbarComponent.className = "navbar-component";
+  navbarComponent.innerHTML = `  
+    <nav class="navbar">
+      <a href="index.html">
+        <img
+          src="/assets/icons/movie-roast-darkbg.svg"
+          alt="Movie-roast-logo"
+          class="logo"
+        />
+      </a>
+      <a href="/pages/reviews/reviews.html">Reviews</a>
+      <a href="/pages/movies/movies.html">Movies</a>
+      <a href="/about.html">About</a>
+      <input id="searchbar" type="text" placeholder="Search..." />
+      <div id="profile-pic-container">
+        <a id="profile-icon" href="/pages/profile/profile.html">
+          <img
+            src="/assets/pictures/profil.jpg"
+            alt="Profile Picture"
+            class="profile-pic"
+          />
+        </a>
+      </div>
+    </nav>
+    <div class="mobile-top-bar">
+      <a href="/index.html">
+        <img
+          src="/assets/icons/movie-roast-darkbg.svg"
+          alt="Movie-roast-logo"
+          class="mobile-logo"
+        />
+      </a>
+    </div>
+    <nav class="mobile-bottom-navbar">
+      <button class="hamburger" id="hamburger-btn">☰</button>
+      <img src="/assets/icons/search.svg" alt="" class="search" />
+      <a id="profile-icon" href="/pages/profile/profile.html">
+        <img
+          src="/assets/pictures/profil.jpg"
+          alt="Profile Picture"
+          class="profile-pic"
+        />
+      </a>
+    </nav>
+    <div id="mobile-menu" class="mobile-menu">
+      <img
+        src="/assets/icons/movie-roast-small.svg"
+        alt="logo hamburger"
+        class="logo-burger"
+      />
+      <div class="links">
+        <a href="/pages/reviews/reviews.html">Reviews</a>
+        <a href="/pages/movies/movies.html">Movies</a>
+        <a href="/about.html">About</a>
+        <a href="/pages/profile/profile.html">Profile</a>
+      </div>
+    </div>
+    `;
+
+    const hamburgerBtn = navbarComponent.querySelector("#hamburger-btn");
+    const mobileMenu = navbarComponent.querySelector("#mobile-menu");
+
+  hamburgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!mobileMenu.contains(e.target) && !hamburgerBtn.contains(e.target)) {
+      mobileMenu.classList.remove("open");
+    }
+  });
+
+  return navbarComponent;
+}
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+import { NavbarComponent } from "/scripts/components/navbar.js";
+
+const navbarElement = NavbarComponent();
+document.getElementById("navbar-component").appendChild(navbarElement);
