2a3900e21bdf33e9f538847f0f498fa02b25b97c
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-05-06 16:05:05 +0200

    Movie page done but some issues with CSS atempt 2

---
+++ scripts/pages/movies/movie.js
@@ -0,0 +1,40 @@
+import { ReviewComponent } from "../../components/reviewComponent.js";
+import { getMovies } from "../../api/movieData.js";
+
+const reviewContainer = document.getElementById("reviews");
+
+const urlParams = new URLSearchParams(window.location.search);
+const movieId = parseInt(urlParams.get("id"), 10);
+const movies = await getMovies();
+
+const movie = movies.find((movie) => movie.id === movieId);
+
+const poster = document.querySelector(".poster");
+const title = document.querySelector(".title");
+const releaseDate = document.querySelector(".releaseDate");
+const director = document.querySelector(".director");
+const stars = document.querySelector(".stars");
+const description = document.querySelector(".description");
+
+poster.src = movie.poster;
+title.innerText = movie.title;
+releaseDate.innerText = movie.releaseDate;
+director.innerText = movie.director;
+stars.innerText = movie.stars;
+description.innerText = movie.description;
+
+const reviews = await fetch("/data/reviews.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const arr = [];
+    data.forEach((review) => {
+      if (review.movie_id === movieId) {
+        arr.push(review);
+      }
+    });
+    return arr;
+  });
+
+reviews.forEach(async (review) => {
+  reviewContainer.appendChild(await ReviewComponent(review));
+});
