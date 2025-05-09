c5b8019516392bf6841bdab442e70586a7d50d1e
greaseburger <nicolai.zackrisson@gmail.com>
2025-05-09 13:15:58 +0200

    reviews and movies sorting, new data to movie object

--- scripts/api/movieData.js
+++ scripts/api/movieData.js
@@ -20,13 +20,33 @@ async function fetchMovies() {
     );
     const data = await res.json();
 
+    const reviews = await fetch("/data/reviews.json")
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => null);
+
     const movies = data.results.map((movie) => {
+      let ratingSum = 0;
+      let totalLikes = 0;
+      let totalReviews = 0;
+      reviews.forEach((review) => {
+        if (review.movie_id === movie.id) {
+          ratingSum += review.rating;
+          totalLikes += review.likes;
+          totalReviews += 1;
+        }
+      });
+      const rating = Math.round(ratingSum / totalReviews);
+
       return {
         id: movie.id,
         title: movie.title,
         description: movie.overview,
         releaseDate: movie.release_date,
         poster: baseUrl + movie.poster_path,
+        rating,
+        totalLikes,
+        totalReviews,
       };
     });
 
@@ -45,6 +65,7 @@ async function fetchMovies() {
 
 export async function getMovies() {
   const cachedMovies = localStorage.getItem("cachedMovies");
+
   if (!cachedMovies) return await fetchMovies();
 
   const movies = JSON.parse(cachedMovies);
--- scripts/pages/movies/movies.js
+++ scripts/pages/movies/movies.js
@@ -3,9 +3,44 @@ import { getMovies } from "../../api/movieData.js";
 
 const trending = document.querySelector(".movies-container#trending");
 const top = document.querySelector(".movies-container#top");
+const sorted = document.querySelector(".movies-container#sorted");
 const page = document.querySelector(".page-content");
+const sections = document.querySelectorAll(".movies-section");
+const sectionTitle = document.querySelector(
+  ".movies-section#sorted .section-title"
+);
 
 const movies = await getMovies();
+
+const urlParams = new URLSearchParams(window.location.search);
+const sort = urlParams.get("sort");
+
+if (sort) {
+  sections.forEach((section) => {
+    if (section.id === "sorted") {
+      section.classList.add("shown");
+    } else {
+      section.classList.add("hidden");
+    }
+  });
+
+  if (sort === "newest") {
+    sectionTitle.innerText = "Newest";
+
+    movies.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate));
+  }
+  if (sort === "trending") {
+    sectionTitle.innerText = "Trending";
+
+    movies.sort((a, b) => b.totalLikes - a.totalLikes);
+  }
+  if (sort === "top") {
+    sectionTitle.innerText = "Most Roasted";
+    movies.sort((a, b) => b.totalReviews - a.totalReviews);
+  }
+
+  sorted.innerHTML = movies.map((movie) => MovieCard(movie)).join("");
+}
 
 //disgusting fix for now
 const trendingMovies = [];
@@ -23,9 +58,36 @@ trending.innerHTML = trendingMovies.map((movie) => MovieCard(movie)).join("");
 top.innerHTML = topMovies.map((movie) => MovieCard(movie)).join("");
 
 page.addEventListener("click", (e) => {
-  const movieId = parseInt(e.target.closest(".movie-card").id, 10);
+  const card = e.target.closest(".movie-card");
+  if (card) {
+    const movieId = parseInt(card.id, 10);
+    if (movieId) {
+      window.location.assign(`movie.html?id=${movieId}`);
+    }
+  }
 
-  if (movieId) {
-    window.location.assign(`movie.html?id=${movieId}`);
+  if (e.target.className === "view-more") {
+    e.preventDefault();
+    console.log(e.target);
+    window.location.assign(`movies.html?sort=${e.target.id}`);
+  }
+});
+
+const sortButton = document.querySelector(".sort-button p");
+const sortMenu = document.querySelector(".sort-menu");
+
+sortButton.addEventListener("click", () => {
+  sortMenu.classList.toggle("open");
+});
+
+sortMenu.addEventListener("click", (e) => {
+  if (e.target.id) {
+    window.location.assign(`movies.html?sort=${e.target.id}`);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!sortMenu.contains(e.target) && !sortButton.contains(e.target)) {
+    sortMenu.classList.remove("open");
   }
 });
