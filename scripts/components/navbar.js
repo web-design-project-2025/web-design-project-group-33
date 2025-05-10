321bb5b904de3ea4d4c36bf495252b0452053525
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-05-10 10:58:06 +0200

    The nav searchbar now shrinks as the page shrinks, however the navbar is not sticky when imported as a component

--- scripts/components/navbar.js
+++ scripts/components/navbar.js
@@ -7,7 +7,7 @@ export function NavbarComponent() {
   const navbarComponent = document.createElement("div");
   navbarComponent.className = "different-navbar-components";
   navbarComponent.innerHTML = `  
-    <nav class="navbar">
+   <nav class="navbar">
       <a href="/index.html">
         <img
           src="/assets/icons/movie-roast-darkbg.svg"
@@ -15,20 +15,20 @@ export function NavbarComponent() {
           class="logo"
         />
       </a>
-      <div>
-      <a href="/pages/reviews/reviews.html">Reviews</a>
-      <a href="/pages/movies/movies.html">Movies</a>
-      <input id="searchbar" type="text" placeholder="Search..." />
+      <div class="nav-links">
+        <a href="/pages/reviews/reviews.html">Reviews</a>
+        <a href="/pages/movies/movies.html">Movies</a>
+        <div class="searchbar-wrapper">
+          <input id="searchbar" type="text" placeholder="Search..." />
+        </div>
       </div>
-      <div id="profile-pic-container">
-        <a id="profile-icon" href="/pages/profile/profile.html">
-          <img
-            src="/assets/pictures/profil.jpg"
-            alt="Profile Picture"
-            class="profile-pic"
-          />
-        </a>
-      </div>
+      <a id="profile-icon" href="/pages/profile/profile.html">
+        <img
+          src="/assets/pictures/profil.jpg"
+          alt="Profile Picture"
+          class="profile-pic"
+        />
+      </a>
     </nav>
     <div class="mobile-top-bar">
       <a href="/index.html">
