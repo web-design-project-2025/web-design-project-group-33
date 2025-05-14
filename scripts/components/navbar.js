76f69b7f271e7350fb226dfbbcb9f1e4321e1fcf
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-05-14 17:42:24 +0200

    On mobile nav the searchbar is now toggled visible when clicking the search button and the logo is hidden

--- scripts/components/navbar.js
+++ scripts/components/navbar.js
@@ -21,7 +21,7 @@ export async function NavbarComponent() {
         <a href="/pages/reviews/reviews.html">Reviews</a>
         <a href="/pages/movies/movies.html">Movies</a>
         <form class="searchbar-wrapper">
-          <input id="searchbar" type="text" placeholder="Search..." value="" />
+          <input id="searchbar" class="searchbar" type="text" placeholder="Search..." value="" />
           <div class="search-menu">
           <ul id="movies"></ul>
           </div>
@@ -37,7 +37,7 @@ export async function NavbarComponent() {
     </nav>
     <nav class="mobile-bottom-navbar">
       <button class="hamburger" id="hamburger-btn">☰</button>
-      <img src="/assets/icons/search.svg" alt="" class="search" />
+      <img src="/assets/icons/search.svg" alt="" class="search" id="search-button"/>
       <a id="profile-icon" href="/pages/profile/profile.html">
         <img
           src="/assets/pictures/profil.jpg"
@@ -78,6 +78,17 @@ export async function NavbarComponent() {
     const movie = e.target.closest(".search-menu ul#movies li");
     if (movie) {
       window.location.assign(`/pages/movies/movie.html?id=${movie.id}`);
+    }
+  });
+
+  const searchButton = navbarComponent.querySelector("#search-button");
+  const logo = navbarComponent.querySelector(".navbar .logo");
+  const searchbarMobile = navbarComponent.querySelector(".navbar .searchbar");
+
+  document.addEventListener("click", (e) => {
+    if (e.target === searchButton) {
+      logo.classList.toggle("logo-hidden");
+      searchbarMobile.classList.toggle("searchbar-visible");
     }
   });
 
